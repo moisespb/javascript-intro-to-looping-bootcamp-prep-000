c417955f52dafa0126b97ffca77b58229d8ab2c3
Moises Perez-Batista <moisesdpbjr@gmail.com>
2018-08-21 16:12:12 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 25; i++) {
+ console.log`"I am ${i} strange loop${i === 0 ? '' : 's'}."`
+ }
+
+for (var i = 0; i < 25; i++) {
+  console.log("I am " + i + " strange loop");
+}
+
+Function whileLoop(i=0) {
+while (let i = 10; i--;)  
+return console.log('done')
+}
+
